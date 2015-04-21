3f0c7f815ca10070dfe4fc4ebdb9fc8d4ec22da1
mainehost <mainehost@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 18:30:18 +0000

    Tagging 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/seo-enforcer/tags/1.2.0@1141032 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seoe_admin.php
+++ seoe_admin.php
@@ -16,8 +16,13 @@ if(!class_exists('seoe_admin')) {
 				<form method="post" action="?page=' . $this->plugin_folder . '">
 				<input type="hidden" name="mode" value="options">
 				<h1>' . SEOE_NAME . '</h1>
+				<h2>Admin Area</h2>
 				<p>
-				<strong>Title Checks:</strong> Enforce title lengths to <input type="text" name="seoe_title_length" size="4" value="' . (($seo_title_length) ? $seo_title_length : 70) . '"> characters.<br />
+				<input type="checkbox" name="seoe_post_notices" value="1"' . ((get_option('seoe_post_notices')) ? ' checked' : '') . '> Enable SEO notices on the admin screens where WordPress SEO or Shopp SEO are used.
+				</p>
+				<h2>Frontend</h2>
+				<p>
+				<strong>Title Checks:</strong> Enforce title lengths to <input type="text" name="seoe_title_length" size="4" value="' . (($seo_title_length) ? $seo_title_length : SEOE_TITLE_LENGTH) . '"> characters.<br />
 				<input type="radio" name="seoe_title" value="1"' . ((get_option('seoe_title')) ? ' checked' : '') . '> Yes <input type="radio" name="seoe_title" value="0"' . ((!get_option('seoe_title')) ? ' checked' : '') . '> No
 				</p>
 				<p>
@@ -29,7 +34,7 @@ if(!class_exists('seoe_admin')) {
 				<strong>Title Truncation Exceptions:</strong> <input type="text" name="seoe_title_trunc_ex" size="50" value="' . get_option('seoe_title_trunc_ex') . '"><br />List of post or page IDs that are an exception to the Title Truncation if enabled. List IDs separated by a comma, IE: 50,99,60. If you want to list your blog index then enter: blog
 				</p>				
 				<p>
-				<strong>Description Checks:</strong> Enforce description lengths to <input type="text" name="seoe_desc_length" size="4" value="' . (($seoe_desc_length) ? $seoe_desc_length : 160) . '"> characters.<br />
+				<strong>Description Checks:</strong> Enforce description lengths to <input type="text" name="seoe_desc_length" size="4" value="' . (($seoe_desc_length) ? $seoe_desc_length : SEOE_DESC_LENGTH) . '"> characters.<br />
 				<input type="radio" name="seoe_desc" value="1"' . ((get_option('seoe_desc')) ? ' checked' : '') . '> Yes <input type="radio" name="seoe_desc" value="0"' . ((!get_option('seoe_desc')) ? ' checked' : '') . '> No
 				</p>
 				<p>
