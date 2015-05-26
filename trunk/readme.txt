8f5fb1fd6181b8a0f31c085cd080a68f355a4327
mainehost <mainehost@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 15:25:52 +0000

    Updating 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/seo-enforcer/tags/1.3.0@1167792 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,161 @@
+=== SEO Enforcer ===
+Contributors: mainehost, godthor
+Tags: SEO, WordPress SEO
+Requires at least: 3.9
+Tested up to: 4.2.2
+Stable tag: 1.3.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Ensures that your title tag and meta description tag aren't too long and truncates them if they are. Requires WordPress SEO by Yoast.
+
+== Description ==
+
+SEO Enforcer is a small plugin that works with WordPress SEO by Yoast (it's required), that will truncate your title tag if it's too long, same with your meta description tag. The length they are truncated at is a setting you can configure.
+
+There is also an option to remove H1 tags from your content. Typically your theme will show the H1 tag for your post or page title already. Having more than one H1 tag is not recommended for SEO, so this will remove any it finds in your content and replace them with an H2 tag instead.
+
+**Plugin Requirements**
+
+* WordPress SEO by Yoast v1.6.1+
+
+**Note:** It may work on earlier versions of those plugins but it has not been tested.
+
+== Installation ==
+
+1. Upload the seo-enforcer folder to the /wp-content/plugins/ directory.
+2. Activate the SEO Enforcer plugin through the 'Plugins' menu in WordPress.
+3. Configure the plugin by going to the SEO Enforcer menu that appears under the Settings menu.
+
+== Frequently Asked Questions ==
+
+None yet.
+
+== Screenshots ==
+
+1. Admin area.
+
+== Changelog ==
+
+= 1.3.0 =
+
+Released: May 26th, 2015
+
+* **New Features**
+	
+	* Image alt and title attribute checks. More info on the settings page.
+
+* **Updates**
+
+	* Reorganized the settings page.
+
+* **Bug Fixes**
+
+	* Fixed a bunch of logic regarding length checks so it should be much better about what it returns.
+	* Title length will now show the saved value instead of always showing the default length.
+
+= 1.2.3 =
+
+Released: May 8th, 2015
+
+* **Updates**
+
+	* If using SEO notices then the length checks will now properly use the lengths you've defined in settings. It will also respect any exceptions you've given.
+	* The SEO notices will now show the recommended lengths and how many characters in excess when it's too long.
+
+* **Bug Fixes**
+	
+	* Fixed an issue where lengths would be shorter than defined if using the ... truncation method.
+	* You can now turn off the SEO notices in settings. It was getting stuck on and wouldn't allow you to disable it.
+	* The SEO notice check will not give an error if length is at the recommended length, only when longer than it.
+
+= 1.2.2 =
+
+Released: May 4th, 2015
+
+* **New Features**
+
+	* Link for rate & review, as well as SEO Enforcer settings, appears on the plugins page.
+
+* **Bug Fixes**
+
+	* Cleaned up some PHP notices about undefined variables.
+
+= 1.2.1 =
+
+Released: April 30th, 2015
+
+* **Bug Fixes**
+
+	* The title and description length checks will now properly truncate after whole words instead of in the middle of a word.
+
+= 1.2.0 =
+
+Released: April 21st, 2015
+
+* **New**
+
+	* Added an option in settings to display SEO notices on admin screens where WordPress SEO or [Shopp SEO](https://wordpress.org/plugins/shopp-seo/) are used. This will give a reminder to manually enter in the SEO fields and will also give errors when content is saved and exceeds the recommended length.
+
+* **Updates**
+
+	* Changed the default title length to 59 characters and description length to 156 to match with WordPress SEO's recommendations.
+
+* **Removed**
+
+	* Cleaned out deprecated code.
+
+= 1.1.1 =
+
+Released: March 31st, 2015
+
+* **Updates**
+
+	* The deactivation of SEO Enforcer if WordPress SEO is deactivated has been removed and instead replaced with an admin notice. The problem was that when WordPress SEO was being upgraded it would in turn deactivate SEO Enforcer. If you did not realize this then you may have had SEO Enforcer deactivated for a while, maybe even now.
+
+* **Bug Fixes**
+
+	* Title and description lengths could be negative in certain situations and in turn create oddities. I now check for negative values to prevent those oddities.
+
+= 1.1.0 =
+
+Released: March 26th, 2015
+
+* **New**
+
+	* Truncation exceptions for titles and descriptions.
+	* Any exception list now accepts *blog* if you want your blog index to be an exception to a rule.
+
+* **Known Issues**
+
+	* Upgrading WordPress SEO will deactivate SEO Enforcer so you have to activate it after the upgrade. I'm hoping to fix this very soon. 
+
+= 1.0.2 =
+
+Released: February 19th, 2015
+
+* Verified compatability with WordPress 4.1.1
+
+= 1.0.1 =
+
+Released: February 10th, 2015
+
+* **Bug Fixes**
+
+	* Fixed an issue where the plugin would not activate if WordPress SEO Premium was installed instead of the free version. It should now activate for either version of WordPress SEO.
+
+= 1.0.0 =
+
+Released: February 5th, 2015
+
+* Initial release of the plugin.
+
+== Upgrade Notice ==
+
+= 1.3.0 =
+
+New feature to check image alt and title attributes.
+
+= 1.2.3 =
+
+Bug fixes and improvements.
---
+++ trunk/seoe_admin.php
@@ -0,0 +1,194 @@
+<?php
+
+if(!class_exists('seoe_admin')) {
+	class seoe_admin {
+		function __construct() {
+			$this->plugin_folder = plugin_basename(dirname(__FILE__));
+		}		
+		function options() {
+			$image_path = plugins_url('images/', __FILE__);
+			$settings = unserialize(get_option('seoe_settings'));
+			extract($settings);
+
+			// $seo_title_length = get_option('seoe_title_length');
+			// $seoe_desc_length = get_option('seoe_desc_length');
+			// $seoe_title_trunc_type = get_option('seoe_title_trunc_type');
+			// $seoe_desc_trunc_type = get_option('seoe_desc_trunc_type');
+
+			define('SEOE_SHOPP_PATH','shopp/Shopp.php'); # Shopp
+			define('SEOE_SSEO_PATH','shopp-seo/sseo.php'); # Shopp SEO
+
+			# See if Shopp is installed but Shopp SEO is not
+			if(in_array(SEOE_SHOPP_PATH, apply_filters('active_plugins', get_option('active_plugins'))) && !in_array(SEOE_SSEO_PATH, apply_filters('active_plugins', get_option('active_plugins')))) {
+				$left_style = 'float: left; width: 73%; margin-right: 2%;';
+				$right_style = 'float: right; width: 20%; border-left: 1px solid #c5c5c5; padding-left: 2%; padding-right: 2%;';
+			}
+			else {
+				$left_style = 'width: 100%';
+				$right_style = 'display: none;';
+			}
+
+			$code = '
+				<style>
+				#seoe_settings .lcol {
+					clear: left;
+					float: left;
+					font-weight: bold;
+					width: 150px;
+					margin-right: 10px;
+					margin-bottom: 10px;
+				}
+				#seoe_settings .rcol {
+					max-width: 50%;
+					float: left;
+					margin-bottom: 10px;
+				}
+				</style>
+
+				<div class="wrap">
+					<form method="post" action="?page=' . $this->plugin_folder . '">
+					<input type="hidden" name="mode" value="options">
+
+					<h1>' . SEOE_NAME . '</h1>
+
+					<h2>Admin Area</h2>
+
+					<div id="seoe_settings">
+						<div style="' . $left_style . '">
+							<div class="lcol">
+								<label for="seoe_post_notices">Enable SEO Notices</label>
+							</div>
+							<div class="rcol">
+								<input type="checkbox" name="seoe_post_notices" id="seoe_post_notices" value="1"' . (($seoe_post_notices) ? ' checked' : '') . '> Yes<br />
+								<em>Show notices on the admin screens where WordPress SEO or Shopp SEO are used.</em>
+							</div>
+
+							<h2 style="clear: both;">Frontend</h2>
+
+							<h3>Title Tag</h3>
+
+							<div class="lcol">
+								<label for="seoe_title">Check:</label>
+							</div>
+							<div class="rcol">
+								<input type="checkbox" name="seoe_title" id="seoe_title" value="1"' . (($seoe_title) ? ' checked' : '') . '> Yes<br />
+								<em>Turns on the title tag check features for the below settings.</em>
+							</div>
+							<div class="lcol">
+								<label for="seoe_title_length">Length:</label>
+							</div>
+							<div class="rcol">
+								<input type="text" name="seoe_title_length" id="seoe_title_length" size="4" value="' . (($seoe_title_length) ? $seoe_title_length : SEOE_TITLE_LENGTH) . '"> characters
+							</div>
+							<div class="lcol">
+								Truncation:
+							</div>
+							<div class="rcol">
+								<input type="radio" name="seoe_title_trunc_type" id="seoe_title_trunc_type1" value="1"' . (($seoe_title_trunc_type == 1) ? ' checked' : '') . '> <label for="seoe_title_trunc_type1">Terminate at character count, IE: This Title is too</label><br />
+								<input type="radio" name="seoe_title_trunc_type" id="seoe_title_trunc_type2" value="2"' . (($seoe_title_trunc_type == 2 || !$seoe_title_trunc_type) ? ' checked' : '') . '> <label for="seoe_title_trunc_type2">Terminate with "...", IE: This Title is too...</label>
+							</div>
+							<div class="lcol">
+								<label for="seoe_title_trunc_ex">Truncation Exceptions:</label>
+							</div>
+							<div class="rcol">
+								<input type="text" name="seoe_title_trunc_ex" id="seoe_title_trunc_ex" size="50" value="' . $seoe_title_trunc_ex . '"><br /><em>List of post or page IDs that are an exception to the Title Truncation if enabled. List IDs separated by a comma.<br />
+								Example: 50,99,60. If you want to list your blog index then enter: blog</em>
+							</div>
+
+							<h3 style="clear: both;">Meta Description</h3>
+
+							<div class="lcol">
+								<label for="seoe_desc">Check:</label>
+							</div>
+							<div class="rcol">
+								<input type="checkbox" name="seoe_desc" id="seoe_desc" value="1"' . (($seoe_desc) ? ' checked' : '') . '> Yes<br />
+								<em>Turns on the title meta description check features for the below settings.</em>
+							</div>
+							<div class="lcol">
+								<label for="seoe_desc_length">Length:</label> 
+							</div>
+							<div class="rcol">
+								<input type="text" name="seoe_desc_length" id="seoe_desc_length" size="4" value="' . (($seoe_desc_length) ? $seoe_desc_length : SEOE_DESC_LENGTH) . '"> characters
+							</div>
+							<div class="lcol">
+								Truncation:
+							</div>
+							<div class="rcol">
+								<input type="radio" name="seoe_desc_trunc_type" id="seoe_desc_trunc_type1" value="1"' . (($seoe_desc_trunc_type == 1) ? ' checked' : '') . '> <label for="seoe_desc_trunc_type1">Terminate at character count, IE: This Description is too</label><br />
+								<input type="radio" name="seoe_desc_trunc_type" id="seoe_desc_trunc_type2" value="2"' . (($seoe_desc_trunc_type == 2 || !$seoe_desc_trunc_type) ? ' checked' : '') . '> <label for="seoe_desc_trunc_type2">Terminate with "...", IE: This Title is too...</label>
+							</div>
+							<div class="lcol">
+								<label for="seoe_desc_trunc_ex">Truncation Exceptions:</label>
+							</div>
+							<div class="rcol">
+								<input type="text" name="seoe_desc_trunc_ex" id="seoe_desc_trunc_ex" size="50" value="' . $seoe_desc_trunc_ex . '"><br />
+								<em>List of post or page IDs that are an exception to the Description Truncation if enabled. List IDs separated by a comma.<br />
+								Example: 50,99,60. If you want to list your blog index then enter: blog</em>
+							</div>
+
+							<h3 style="clear: both;">H1</h3>
+
+							<div class="lcol">
+								<label for="seoe_h1">Check:</label>
+							</div>
+							<div class="rcol">
+								<input type="checkbox" name="seoe_h1" id="seoe_h1" value="1"' . (($seoe_h1) ? ' checked' : '') . '> Yes<br />
+								<em>Enforce no H1 tags in the content since it should be done by the theme. This will replace any H1 tags in content with an H2 tag instead.</em>
+							</div>
+							<div class="lcol">
+								<label for="seoe_h1_ex">Exceptions:</label>
+							</div>
+							<div class="rcol">
+								<input type="text" name="seoe_h1_ex" id="seoe_h1_ex" size="50" value="' . $seoe_h1_ex . '"><br />
+								<em>List of post or page IDs that are an exception to the HQ check if enabled. List IDs separated by a comma, IE: 50,99,60. If you want to list your blog index then enter: blog</em>
+							</div>
+
+							<h3 style="clear: both;">Image</h3>
+
+							<div class="lcol">
+								<label for="seoe_img">Check:</label> 
+							</div>
+							<div class="rcol">
+								<input type="checkbox" name="seoe_img" id="seoe_img" value="1"' . (($seoe_img) ? ' checked' : '') . '> Yes<br />
+								<em>This will check if your images have the alt and title attribute.<br />
+									If neither is present then an alt and title will be created using the image name.<br />
+									If either an alt or title is present, but not both, then the missing attribute will be created using the value of the one that is present.</em>
+							</div>
+							<p style="clear: both;">
+							<input type="submit" value="Save" class="button button-primary">
+							</form>
+							</p>
+						</div>
+						<div style="' . $right_style . '">
+							<a href="https://wordpress.org/plugins/shopp-seo/" target="_blank"><img src="' . $image_path . 'shopp_seo.png" style="max-width: 100%;"></a>
+							<p>
+							If you use Shopp then check out <a href="https://wordpress.org/plugins/shopp-seo/" target="_blank">Shopp SEO</a> to fix the integration of WordPress SEO and Shopp.						
+							</p>
+						</div>
+					</div>
+				</div>';
+
+			echo $code;
+		}
+	}
+}
+
+$seoe_admin = new seoe_admin();
+
+switch($mode) {
+	default:
+		if($_POST) {
+			foreach($_POST as $key=>$value) {
+				if($key != 'mode' && $key != 'submit') {
+					$settings[$key] = $value;
+				}
+			}
+
+			update_option('seoe_settings', serialize($settings));
+		}
+
+		$seoe_admin->options();
+	break;
+}
+
+?>
